a46e193792c083538c2ed5053afc8fe088595362
satyasai69 <ch.satya.sai133@gmail.com>
2023-11-05 19:50:32 +0530

    owner and address

--- functionmodifier.sol
+++ functionmodifier.sol
@@ -34,22 +34,4 @@ contract FunctionModifier {
         owner = _newOwner;
     }
 
-    // Modifiers can be called before and / or after a function.
-    // This modifier prevents a function from being called while
-    // it is still executing.
-    modifier noReentrancy() {
-        require(!locked, "No reentrancy");
-
-        locked = true;
-        _;
-        locked = false;
-    }
-
-    function decrement(uint i) public noReentrancy {
-        x -= i;
-
-        if (i > 1) {
-            decrement(i - 1);
-        }
-    }
 }
